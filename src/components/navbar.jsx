a96cbdd8578b96b21a6ffba975726605f3845c33
SISIR SEN <sisirsen22122004@gmail.com>
2026-04-20 10:00:00 +0530

    navbar & html page update

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,8 +3,8 @@ import {icons} from '../../utils/constants'
 
 function Navbar() {
   return (
-    <div className='p-8 flex items-center gap-65'>
-      <div className=' flex cursor-pointer gap-2 text-2xl text-orange-400 font-bold items-center'><img className='h-10 w-12' src={icons.logo} alt="logo" />Voynex</div>
+    <div className='p-8 flex items-center gap-50'>
+      <div className=' flex pl-15 cursor-pointer gap-2 text-3xl text-orange-400 font-bold items-center'><img className='h-10 w-12' src={icons.logo} alt="logo" />Voynex</div>
 
       <div className='list-none flex gap-15 text-lg text-orange-500 cursor-pointer '>
 
@@ -22,8 +22,8 @@ function Navbar() {
       hover:after:w-full">About Us</li>
       </div>
 
-      <div className='flex gap-5'>
-        <div className=' hover:bg-orange-500 bg-orange-600 p-2 px-2 rounded-md cursor-pointer text-white font-semibold'>Log In</div>
+      <div className='flex gap-10'>
+        <div className=' hover:bg-orange-500 bg-orange-600 p-2 rounded-md cursor-pointer text-white font-semibold'>Log In</div>
         <div className=' hover:bg-orange-500 bg-orange-600 p-2 rounded-md cursor-pointer text-white font-semibold'>Sign Up</div>
       </div>
     </div>
