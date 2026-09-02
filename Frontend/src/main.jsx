fd82c02a11c87d037d12763609269225d88e4f51
SISIR SEN <sisirsen22122004@gmail.com>
2026-09-02 19:26:59 +0530

    some fixes

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,16 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-    
+createRoot(document.getElementById('root')).render(
+<BrowserRouter>   
  <App />
- 
-   
-
-    </BrowserRouter>
-    
-  
+</BrowserRouter> 
 )
